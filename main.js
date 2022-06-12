261cb0915e644897b0ff9d62eeab1100bc466675
mathieu_muller <pere.cil@gmail.com>
2022-06-12 16:42:19 +0200

    Remove jquery

--- main.js
+++ main.js
@@ -1,79 +1,88 @@
 window.data = [];
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then((function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }), function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').then(
+      registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
+      err => console.log('ServiceWorker registration failed: ', err)
+    );
+  });
 }
 
-$( document ).ready( function() 
-{
-    function loadStratagems(data) {
+window.addEventListener('load', () => {
 
-        $( '#setup ol' ).html('');
-        $( '#sidebar' ).html('');
-        $('article').html('');
+  let loadStratagems = function (data) {
 
-        for(const stratagem of data.stratagems)
-        {
-            const className = stratagem.title.toLowerCase().replaceAll(' ', '-');
-            const typeClass = stratagem.type.toLowerCase().replace(' stratagem', '').replaceAll(' ', '-');
-            const state = ( window.localStorage.getItem(className) ?? "true" ) === "true";
+    document.querySelector('#setup ol').innerHTML = '';
+    document.querySelector('#sidebar').innerHTML = '';
+    document.querySelector('article').innerHTML = '';
 
-            $( '#setup ol' ).append( '<li><input type="checkbox" class="switch" id="switch-' + className + '" autocomplete="off" ' + ( state === true ? 'checked="checked" ' : ' ' ) + '/><label for="switch-' + className + '" data-class="' + className + '">Show "' + stratagem.title + '" on the list</label>' + stratagem.description + '</li>' );
-            $( '#sidebar' ).append( '<a href="#' + className + '" class="' + className + ( state === true ? '' : ' hidden' ) + '">' + stratagem.title + '</a>' );
-
-            let html = "";
-
-            html += '<div class="stratagem ' + className + ( state === true ? '' : ' hidden' ) + '">';
-            html += '   <span class="cost">' + stratagem.min_cost + 'CP' + ( stratagem.max_cost ? '/' + stratagem.max_cost + 'CP' : '' ) + '</span>';
-            html += '   <h2 id="' + className + '" class="' + typeClass + '">' + stratagem.title + '</h2>';
-            html += '   <p class="type">' + stratagem.type + ( stratagem.specialism === null ? '' : ' - ' + stratagem.specialism ) +  '</p>';
-            html += '   <section class="description">' + stratagem.description + '</section>';
-            html += '</div>';
-
-            $( 'article' ).append( html );
-        }
-    }
-
-    $( '#setup' ).on( 'click', 'ol label', function() {
-        let className = $(this).data('class');
-        let state = $( this ).prev().prop( 'checked' );
-
-        window.localStorage.setItem( className, (!state).toString() );
-        $( '.' + className ).toggleClass( 'hidden', state );
-    });
-
-    $( '#sidebar' ).on( 'click', 'a', function() {
-        $( '#sidebar-collapse' ).prop( 'checked', false );
-    });
-
-    $( '#setup' ).on('click', '[data-source]', function() {
-        window.localStorage.setItem('faction', $(this).data('source'));
-        loadStratagems(window.data[$(this).data('source')]);
-    });
-
-    for( var i = 0; i < window.data.length; i++ ) 
+    for(const stratagem of data.stratagems)
     {
-        let faction = window.data[i];
-        let button = '';
-        
-        button += '<input type="radio" name="faction" id="' + faction.id + '" autocomplete="off" />';
-        button += '<label for="' + faction.id + '" data-source="' + i + '">';
-        button += '  <img src="resources/img/factions/' + faction.id + '.svg" title="'+ faction.faction +'" />';
-        button += '  <span>' + faction.faction + '</span>';
-        button += '</label>';
+      const className = stratagem.title.toLowerCase().replaceAll(' ', '-');
+      const typeClass = stratagem.type.toLowerCase().replace(' stratagem', '').replaceAll(' ', '-');
+      const state = ( window.localStorage.getItem(className) ?? "true" ) === "true";
 
-        $( '#factions' ).append( button );
+      document.querySelector('#setup ol').innerHTML += '<li><input type="checkbox" class="switch" id="switch-' + className + '" autocomplete="off" ' + ( state === true ? 'checked="checked" ' : ' ' ) + '/><label class="configuration-item" id="label-switch-' + className + '" for="switch-' + className + '" data-class="' + className + '">Show "' + stratagem.title + '" on the list</label>' + stratagem.description + '</li>';
+      document.querySelector('#sidebar').innerHTML += '<a href="#' + className + '" class="' + className + ( state === true ? '' : ' hidden' ) + '">' + stratagem.title + '</a>';
+
+      let html = "";
+
+      html += '<div class="stratagem ' + className + ( state === true ? '' : ' hidden' ) + '">';
+      html += '   <span class="cost">' + stratagem.min_cost + 'CP' + ( stratagem.max_cost ? '/' + stratagem.max_cost + 'CP' : '' ) + '</span>';
+      html += '   <h2 id="' + className + '" class="' + typeClass + '">' + stratagem.title + '</h2>';
+      html += '   <p class="type">' + stratagem.type + ( stratagem.specialism === null ? '' : ' - ' + stratagem.specialism ) +  '</p>';
+      html += '   <section class="description">' + stratagem.description + '</section>';
+      html += '</div>';
+
+      document.querySelector('article').innerHTML += html;
     }
 
-    const selectedFaction = parseInt( ( window.localStorage.getItem('faction') ?? "0" ), 10);
+  }
 
-    $( '#setup input[type="radio"]:eq(' + selectedFaction + ')' ).prop( 'checked', true );
+  document.getElementById( 'sidebar' ).addEventListener( 'click', (e) => {
+    if (e.target && e.target.matches('a')) {
+      document.getElementById( 'sidebar-collapse' ).checked = false;
+    }
+  });
 
-    loadStratagems(window.data[selectedFaction]);
-});
+  document.getElementById( 'setup' ).addEventListener( 'click', (e) => {
+
+    if (e.target && e.target.matches('.configuration-item')) {
+      let className = e.target.getAttribute( 'data-class' );
+      let state = e.target.previousElementSibling.checked;
+
+      window.localStorage.setItem( className, (!state).toString() );
+
+      for(let target of document.querySelectorAll( '.' + className )) {
+        target.classList.toggle( 'hidden', state );
+      }
+    }
+
+    if (e.target && e.target.matches('[data-source]')) {
+      let source = e.target.getAttribute( 'data-source');
+      window.localStorage.setItem('faction', source);
+      loadStratagems(window.data[parseInt(source,10)]);
+    }
+  });
+
+  for( var i = 0; i < window.data.length; i++ ) 
+  {
+      let faction = window.data[i];
+      let button = '';
+      
+      button += '<input type="radio" name="faction" id="' + faction.id + '" autocomplete="off" />';
+      button += '<label id="label-' + faction.id + '" for="' + faction.id + '">';
+      button += '  <img src="resources/img/factions/' + faction.id + '.svg" title="'+ faction.faction +'" data-source="' + i + '" />';
+      button += '  <span>' + faction.faction + '</span>';
+      button += '</label>';
+
+      document.getElementById( 'factions' ).innerHTML += button;
+  }
+  
+  const selectedFaction = parseInt( ( window.localStorage.getItem('faction') ?? "0" ), 10);
+
+  document.querySelectorAll( '#setup input[type="radio"]' ).item( selectedFaction ).checked = true;
+
+  loadStratagems(window.data[selectedFaction]);
+})
--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('stratablet-v1').then((cache) => cache.addAll([
-      '/index.html',
-      '/data/craftworlds.js',
-      '/data/spacemarines.js',
-      '/data/tau.js',
-      '/main.js',
-      '/style.css',
-      '/resources/fonts/Conduit-ExtraBold.otf',
-      '/resources/fonts/Conduit-Regular.ttf',
-      '/resources/fonts/Conduit-Bold.ttf',
-      '/resources/fonts/Conduit-BoldItalic.ttf',
-      '/resources/fonts/Conduit-ExtraLight.ttf',
-      '/resources/fonts/Conduit-Light.ttf',
-      '/resources/fonts/Conduit-LightItalic.ttf',
-      '/resources/fonts/Conduit-Medium.ttf',
-      '/resources/fonts/Conduit-MediumItalic.ttf',
-      '/resources/icons/icon-64.png',
-      '/resources/icons/icon-128.png',
-      '/resources/icons/icon-256.png',
-      '/resources/icons/icon-512.png'
+      'index.html',
+      'main.js',
+      'data/craftworlds.js',
+      'data/spacemarines.js',
+      'data/tau.js',
+      'style.css',
+      'resources/fonts/Conduit-ExtraBold.otf',
+      'resources/fonts/Conduit-Regular.ttf',
+      'resources/fonts/Conduit-Bold.ttf',
+      'resources/fonts/Conduit-BoldItalic.ttf',
+      'resources/fonts/Conduit-ExtraLight.ttf',
+      'resources/fonts/Conduit-Light.ttf',
+      'resources/fonts/Conduit-LightItalic.ttf',
+      'resources/fonts/Conduit-Medium.ttf',
+      'resources/fonts/Conduit-MediumItalic.ttf',
+      'resources/icons/64.png',
+      'resources/icons/128.png',
+      'resources/icons/256.png', 
+      'resources/icons/512.png'
     ])),
   );
 });
@@ -30,56 +30,3 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
-
-
-/*
-console.log('[Service Worker] Install');
-debugger;
-
-const cacheName = 'stratablet-v1';
-
-const appShellFiles = [
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
-    '/index.html',
-    '/data/craftworlds.js',
-    '/data/spacemarines.js',
-    '/data/tau.js',
-    '/main.js',
-    '/style.css',
-    '/resources/fonts/Conduit-ExtraBold.otf',
-    '/resources/fonts/Conduit-Regular.ttf',
-    '/resources/fonts/Conduit-Bold.ttf',
-    '/resources/fonts/Conduit-BoldItalic.ttf',
-    '/resources/fonts/Conduit-ExtraLight.ttf',
-    '/resources/fonts/Conduit-Light.ttf',
-    '/resources/fonts/Conduit-LightItalic.ttf',
-    '/resources/fonts/Conduit-Medium.ttf',
-    '/resources/fonts/Conduit-MediumItalic.ttf',
-    '/resources/icons/icon-64.png',
-    '/resources/icons/icon-128.png',
-    '/resources/icons/icon-256.png',
-    '/resources/icons/icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(appShellFiles);
-    })());
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-      const r = await caches.match(e.request);
-      console.log('[Service Worker] Fetching resource: ${e.request.url}');
-      if (r) { return r; }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching new resource: ${e.request.url}');
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
-*/
